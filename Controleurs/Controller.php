ae95bf02ce56e4f482c2946a306a8595126bdbbf
Alexandre <alexandre@brde.fr>
2018-01-22 16:27:33 +0100

    Fixe des require()

--- Controleurs/Controller.php
+++ Controleurs/Controller.php
@@ -6,24 +6,24 @@
  * Time: 02:34
  */
 
-require('../Modeles/Modele.php');
+require('Modeles/Modele.php');
 
 function affTest()
 {
     $contenu = test();
-    require('../Vues/vueTest.php');
+    require('Vues/vueTest.php');
 }
 
 function affTestParam()
 {
     $contenu = testParam($_GET['id']);
-    require('../Vues/vueTest.php');
+    require('Vues/vueTest.php');
 }
 
 function affAccueil()
 {
     $contenu = testAccueil();
-    require('../Vues/vueAccueil.php');
+    require('Vues/vueAccueil.php');
 }
 
 ?>
