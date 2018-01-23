d7c6bf007f3195987a1268c902d5a4219dee5290
Alexandre <chronoos89@gmail.com>
2018-01-23 23:14:24 +0100

    Informations profil

--- Vues/vueMembre.php
+++ Vues/vueMembre.php
@@ -4,12 +4,21 @@ $titre = $membre['pseudo'];
 
 ob_start(); ?>
 
-<div class="jumbotron jumbotron-fluid jumbotron-header bg-primary text-white">
-    <h1 class="display-3 text-center jumbotron-title"><?= $membre['pseudo'] ?></h1>
-    <img src="ImageProfil/<?= $membre['image']; ?>.png" class="rounded mx-auto d-block image-profil" alt="<?= $membre['pseudo']; ?>">
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="ImageProfil/<?= $membre['image']; ?>.png" class="rounded mx-auto d-block image-profil" alt="<?= $membre['pseudo']; ?>">
+        </div>
+        <div class="col col-lg-5hop">
+            <p><?= $membre['pseudo'] ?></p>
+            <p>Phrase de statut</p>
+            <p>Sexe</p>
+            <p>Âge</p>
+            <p>Localisation</p>
+            <p>Dernière connexion</p>
+        </div>
+    </div>
 </div>
-
-
 
 <?php $contenu = ob_get_clean(); ?>
 
